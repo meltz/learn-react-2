9bde468b6f47334c6ebcd65cb0dd6945fafeaf9b
mz <mz@mz.com>
2022-11-07 17:08:38 +0800

    useState React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+/* https://fabisiakradoslaw.medium.com/understand-react-hooks-in-a-few-minutes-and-use-it-from-now-5020df968486 */
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [balance, setBalance] = useState(1500);
+  const [savingBalance, setSavingBalance] = useState(500);
+
+  const handleSaving = () => {
+    setBalance(balance - 100);
+    setSavingBalance(savingBalance + 100);
+  };
+
+  const handleSpending = () => {
+    setBalance(balance + 100);
+    setSavingBalance(savingBalance - 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='col balance-section'>
+        <h4 className='title'>My Balance</h4>
+        <p>${balance}</p>
+        <button className='btn' onClick={() => handleSaving()}>
+          Spend $100
+        </button>
+      </div>
+      <div className='col saving-balance-section'>
+        <h4 className='title'>My Saving Balance</h4>
+        <p>${savingBalance}</p>
+        <button className='btn' onClick={() => handleSpending()}>
+          Spend $100
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
